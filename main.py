f4a1c5873f8492b37c09b127f975172c24f2111a
yunjey <yunjey47@naver.com>
2019-01-22 11:16:52 +0000

    fixed the download link

--- main.py
+++ main.py
@@ -91,8 +91,8 @@ if __name__ == '__main__':
     parser.add_argument('--use_tensorboard', type=str2bool, default=True)
 
     # Directories.
-    parser.add_argument('--celeba_image_dir', type=str, default='data/CelebA_nocrop/images')
-    parser.add_argument('--attr_path', type=str, default='data/list_attr_celeba.txt')
+    parser.add_argument('--celeba_image_dir', type=str, default='data/celeba/images')
+    parser.add_argument('--attr_path', type=str, default='data/celeba/list_attr_celeba.txt')
     parser.add_argument('--rafd_image_dir', type=str, default='data/RaFD/train')
     parser.add_argument('--log_dir', type=str, default='stargan/logs')
     parser.add_argument('--model_save_dir', type=str, default='stargan/models')
