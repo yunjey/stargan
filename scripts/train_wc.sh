67814d725a0d457d89367b5e8e316418adaa8f76
Akanksha Atrey <akanksha.atrey@gmail.com>
2019-11-05 21:34:54 -0500

    update + adding script to train wc

--- scripts/train_wc.sh
+++ scripts/train_wc.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #
-#SBATCH --job-name=train_stargan_transient
-#SBATCH --output=stargan_transient/res_%j.txt  # output file
-#SBATCH -e stargan_transient/res_%j.err        # File to which STDERR will be written
+#SBATCH --job-name=train_stargan_wc
+#SBATCH --output=stargan_wc/res_%j.txt         # output file
+#SBATCH -e stargan_wc/res_%j.err               # File to which STDERR will be written
 #SBATCH --partition=titanx-long         	   # Partition to submit to
 #
 #SBATCH --mem=32000                     	   # Memory required in MB
@@ -16,7 +16,7 @@ echo "Start running experiments"
 
 source ~/.bashrc ; source activate tf
 
-python code/main.py --mode train --dataset RaFD --c_dim 40 --rafd_image_dir data/transient_attributes/train --sample_dir stargan_transient/samples --log_dir stargan_transient/logs --model_save_dir stargan_transient/models --result_dir stargan_transient/results
+python code/main.py --mode train --dataset RaFD --c_dim 40 --rafd_image_dir data/world_cities/train --sample_dir stargan_wc/samples --log_dir stargan_wc/logs --model_save_dir stargan_wc/models --result_dir stargan_wc/results
 
 echo "Done"
 
