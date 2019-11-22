ef3169e30a5468c46467db57930b295ebaaf67f6
Akanksha Atrey <akanksha.atrey@gmail.com>
2019-11-22 14:04:02 -0500

    created script to train seasonal data only (ie. certain attributes)

--- scripts/train_seasonal.sh
+++ scripts/train_seasonal.sh
@@ -14,9 +14,9 @@ echo "SLURM_JOBID: " $SLURM_JOBID
 
 echo "Start running experiments"
 
-# source ~/.bashrc ; source activate tf
+source venv/bin/activate
 
-python3 code/main.py --mode train --dataset RaFD --c_dim 14 --rafd_image_dir data/transient_attributes/train --sample_dir stargan_seasonal/samples \
+python code/main.py --mode train --dataset RaFD --c_dim 14 --rafd_image_dir data/transient_attributes/train --sample_dir stargan_seasonal/samples \
 		 --log_dir stargan_seasonal/logs --model_save_dir stargan_seasonal/models --result_dir stargan_seasonal/results \
 		 --selected_attrs daylight night sunrisesunset dawndusk sunny clouds fog storm snow spring summer autumn winter rain
 
