90b933ccf321fa1d45f2b3a3617456101f730f17
Vinita <vinita.hissaria@tsgforce.com>
2019-11-23 18:49:58 -0500

    Changing the resize dims to (128, 128) so that we get the correct size images

--- code/data_loader.py
+++ code/data_loader.py
@@ -75,7 +75,7 @@ def get_loader(image_dir, attr_path, selected_attrs, crop_size=178, image_size=1
     if mode == 'train':
         transform.append(T.RandomHorizontalFlip())
     # transform.append(T.CenterCrop(crop_size))
-    transform.append(T.Resize(image_size))
+    transform.append(T.Resize(image_size, image_size))
     transform.append(T.ToTensor())
     transform.append(T.Normalize(mean=(0.5, 0.5, 0.5), std=(0.5, 0.5, 0.5)))
     transform = T.Compose(transform)
